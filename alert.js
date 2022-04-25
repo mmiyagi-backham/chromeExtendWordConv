c7590d50780a7417b03457c2023a3d8fad3caf63
Michihiro Miyagi <mmiyagi@mmiyagiBackhamMbp.local>
2022-04-25 18:16:00 +0900

    新規追加 - html/css

--- alert.js
+++ alert.js
@@ -1,9 +1,6 @@
 function showAlert() {
   alert("今日も楽しく開発しよう！");
 }
-chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: showAlert,
-  });
-});
+document.getElementById("alertButton").onclick = function () {
+  showAlert();
+};
